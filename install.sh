4942dedbac2c4e63132d68159d70f711755e7406
Slayers99 <88161494+Slayers99@users.noreply.github.com>
2023-12-04 22:37:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Ścieżka do tymczasowego pliku klucza
-TEMP_KEY_FILE="/tmp/repo_key.asc"
+# Pobranie ścieżki do bieżącego katalogu
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Adres URL pliku klucza na GitHubie
-KEY_URL="https://github.com/Slayers99/public_keys/blob/main/pub_key.asc"
-
-# Pobieranie klucza publicznego z GitHuba
-wget -O "$TEMP_KEY_FILE" "$KEY_URL"
+# Ścieżka do klucza publicznego w lokalnym repozytorium
+LOCAL_KEY_PATH="$CURRENT_DIR/pub_key.asc"
 
 # Dodawanie klucza publicznego do systemu APT
-sudo apt-key add "$TEMP_KEY_FILE"
-
-# Opcjonalnie: usuwanie tymczasowego pliku klucza
-rm -r "$TEMP_KEY_FILE"
+sudo apt-key add "$LOCAL_KEY_PATH"
